fc8c63845f575d8f9eea796bf6d93498c601b751
Viștianu Vladimir <111700103+HuperNacks@users.noreply.github.com>
2022-09-05 20:39:40 +0300

    Update Program.cs

--- UDP.Client/Program.cs
+++ UDP.Client/Program.cs
@@ -1,6 +1,6 @@
 ﻿using UDP.Client;
 
-ExpressEncription.RSAEncription.MakeKey(@"D:\programare\Max volosenko\Task\UDP.Client\public.key", @"D:\programare\Max volosenko\Task\UDP.Client\private.key");
+ExpressEncription.RSAEncription.MakeKey(@"D:\Local\Task\UDP.Client\public.key", @"D:\Local\Task\UDP.Client\private.key");
 UDPManager client = new UDPManager();
 
 client.Start();
